4e9ee517f7cc7b2679a0ea7b691cd0523bacefb0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-23 12:21:49 +0100

    Creazione progetto Equazioni determinate e impossibili

--- EquazioniLibrary.Test/Equazioni_Test.cs
+++ EquazioniLibrary.Test/Equazioni_Test.cs
@@ -1,19 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
 
 namespace EquazioniLibrary.Test
 {
     [TestClass]
     public class Equazioni_Test
     {
+
         [TestMethod]
         public void TestMethod1()
         {
+
             double a = 3;
             double b = 4;
             bool risposta_aspettata = true;
             bool risposta = Equazioni.IsDetermined(a, b);
             Assert.AreEqual(risposta_aspettata, risposta);
+        }
+            
+            [TestMethod]
+        public void TestMethod2()
+        {
+            double a = 3;
+                double b = 4;
+                bool risposta2_aspettata = true;
+                bool risposta2 = Equazioni.IsInconsisted(a, b);
+                Assert.AreEqual(risposta2_aspettata, risposta2);
+            
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -8,9 +8,19 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
-        public static void IsDetermined(double a; double b)
+        public static bool IsDetermined(double a, double b)
         {
-            
+            bool risposta = false;
+            if (a != 0)
+                risposta = true;
+            return risposta;
+        }
+        public static bool IsInconsisted(double a, double b)
+        {
+            bool risposta2 = false;
+            if (a == 0)
+                risposta2 = true;
+            return false;
         }
     }
 }
